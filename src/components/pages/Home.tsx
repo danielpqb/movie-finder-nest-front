6e6baa6b6800e4652a79ac44f11cb851ac22f0ba
danielpqb <danielpqb@gmail.com>
2023-03-07 14:19:07 -0300

    bg-fix

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -2,11 +2,18 @@ import { Animation } from "react-animate-with-css";
 import Header from "../others/Header/Header";
 import Info from "../others/Info/Info";
 import Movie from "../others/Movie/Movie";
+import bgImg from "../../assets/bg-waves.svg";
+
 import "./index.scss";
 
 export default function Home() {
   return (
     <div className="home">
+      <img
+        src={bgImg}
+        className="home-bg"
+      />
+
       <Header />
 
       <Info />
